3a2d142c02b2982521be166deb551b6f3d9830a2
Jalal-Amourgha <myproo100@gmail.com>
2023-10-06 14:47:02 +0100

    New Design

--- js.js
+++ js.js
@@ -1,76 +1,129 @@
-// const score = JSON.parse(localStorage.getItem(score))
+let newDesign = false;
+let images = document.querySelector(".image-boxs");
+let playerImgPicked = document.getElementById("playerImgPicked")
+let computerImgPicked = document.getElementById("computerImgPicked")
+let computerMove = 0;
+let result = "";
+let score = document.getElementById("score");
+let wins = 0;
+let losses = 0;
+let ties = 0;
 
-const score = {
-    wins: 0,
-    losses: 0,
-    ties: 0
-}
 
-function resetScore() {
-    document.querySelector('.js-score').innerHTML = `  Wins : ${score.wins} , Losses : ${score.losses} and Ties : ${score.ties} `;
-}
 
-function random() {
-    let computerMove;
-    let computerPick = Math.random();
 
-    if (computerPick > 0 && computerPick < 1 / 3) {
-        computerMove = 'rock';
+images.onclick = function (e) {
 
-    } else if (computerPick > 1 / 3 && computerPick < 2 / 3) {
-        computerMove = 'paper';
+    //Change The Design
+    if (newDesign === false) {
+        document.getElementsByClassName("first-design")[0].classList.add("container");
+        document.getElementsByClassName("first-design")[0].classList.remove("first-design");
+        document.getElementsByClassName("box-two")[0].classList.remove("display-none")
+        document.getElementsByClassName("image-boxs")[0].style.flexDirection = "column";
+        document.getElementsByClassName("choose")[0].remove()
 
-    } else if (computerPick > 2 / 3) {
-        computerMove = 'scissor';
+        newDesign = true;
+    };
+
+    //Take The Source Of The Image Clicked
+    let src;
+    src = e["target"]["id"];
+
+
+    //Display The Image That User Choose
+    playerImgPicked.src = `images/${src}.jpg`;
+
+
+
+    //Style The Border of The Image
+    if (src == "rock") {
+        playerImgPicked.style.borderColor = "var(--blue)";
+    } else if (src == "paper") {
+        playerImgPicked.style.borderColor = "var(--red)";
+    } else {
+        playerImgPicked.style.borderColor = "var(--green)";
     }
+
+
+    //Call The Functio That Generate Computer Move
+    computerPick();
+
+    //Display The Computer Move
+    if (computerMove == 0) {
+        computerImgPicked.src = `images/rock.jpg`;
+        computerImgPicked.style.borderColor = "var(--blue)";
+    } else if (computerMove == 1) {
+        computerImgPicked.src = `images/paper.jpg`;
+        computerImgPicked.style.borderColor = "var(--red)"
+    } else {
+        ;
+        computerImgPicked.src = `images/scissors.jpg`;
+        computerImgPicked.style.borderColor = "var(--green)";
+    }
+    
+
+    //Finding The Winner
+    if (computerMove === 0) {
+        if (computerMove == 0 && src == "rock") {
+            result = "Tie!";
+            ties++;
+        } else if (computerMove == 0 && src == "paper") {
+            result = "Win!";
+            wins++;
+        } else if (computerMove == 0 && src == "scissors") {
+            result = "Lost!";
+            losses++;
+        }
+    } else if (computerMove === 1) {
+        if (computerMove == 1 && src == "rock") {
+            result = "Lost!";
+            losses++;
+        } else if (computerMove == 1 && src == "paper") {
+            result = "Tie!";
+            ties++;
+        } else if (computerMove == 1 && src == "scissors") {
+            result = "Win!";
+            wins++;
+        }
+    } else {
+        if (computerMove == 2 && src == "rock") {
+            result = "Win!";
+            wins++;
+        } else if (computerMove == 2 && src == "paper") {
+            result = "Lost!";
+            losses++;
+        } else if (computerMove == 2 && src == "scissors") {
+            result = "Tie!";
+            ties++;
+        }
+    }
+    
+
+
+    //Display The Result
+    if (result === "Win!") {
+        document.getElementById("result").innerHTML = `You ${result}`;
+    } else if (result === "Lost!") {
+        document.getElementById("result").innerHTML = `You ${result}`;
+    } else {
+        document.getElementById("result").innerHTML = result;
+    }
+    
+
+    //Display The Score
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("wins").style.color = "var(--green)";
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("losses").style.color = "var(--red)";
+    document.getElementById("ties").innerHTML = ties;
+    document.getElementById("ties").style.color = "var(--blue)";
+};
+
+//The Function generate The Computer Move
+function computerPick() {
+    
+    computerMove = Math.floor(Math.random() * 3);
 
     return computerMove;
+};
 
-}
-
-function playerGame(playerMove) {
-    const computerMove = random();
-    let result;
-
-    if (playerMove === 'rock') {
-        if (computerMove === 'rock') {
-            result = 'Tie!';
-        } else if (computerMove === 'paper') {
-            result = 'You Lost!';
-        } else if (computerMove === 'scissor') {
-            result = 'You Win!';
-        }
-    } else if (playerMove === 'paper') {
-        if (computerMove === 'paper') {
-            result = 'Tie!';
-        } else if (computerMove === 'scissor') {
-            result = 'You Lost!';
-        } else if (computerMove === 'rock') {
-            result = 'You Win!';
-        }
-    } else if (playerMove === 'scissor') {
-        if (computerMove === 'scissor') {
-            result = 'Tie!';
-        } else if (computerMove === 'rock') {
-            result = 'You Lost!';
-        } else if (computerMove === 'paper') {
-            result = 'You Win!';
-        }
-    }
-    document.querySelector('.js-picks').innerHTML = `  You picked : ${playerMove}       and     Computer picked : ${computerMove} `;
-
-    if (result === 'Tie!') {
-        score.ties += 1;
-    } else if (result === 'You Lost!') {
-        score.losses += 1;
-    } else if (result === 'You Win!') {
-        score.wins += 1;
-    }
-
-
-
-    document.querySelector('.js-result').innerHTML = ` ${result} `;
-    document.querySelector('.js-score').innerHTML = `  Wins : ${score.wins} , Losses : ${score.losses} and Ties : ${score.ties}`;
-
-    localStorage.setItem('score', JSON.stringify(score));
-}
